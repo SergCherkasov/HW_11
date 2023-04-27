628dd2b6cfe0c5a9769f23c96fece73957a5b06f
Admin <Admin@DESKTOP-34GRT87>
2023-04-27 13:30:40 +0300

    Add project files.

---
+++ HW_11/MergeSort.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_11
+{
+    internal class MergeSort
+    {
+        public int[] SortArray(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+
+                Task task = new Task(() => { SortArray(array, left, middle); /*Thread.Sleep(200);*/ });
+                Task task1 = task.ContinueWith(t => SortArray(array, middle + 1, right));
+                Task task2 = task1.ContinueWith(t => MergeArray(array, left, middle, right));
+
+                task.Start();
+                task2.Wait();
+            }
+
+            return array;
+        }
+
+        public void MergeArray(int[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new int[leftArrayLength];
+            var rightTempArray = new int[rightArrayLength];
+            int i, j;
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+            i = 0;
+            j = 0;
+            int k = left;
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+        public void ShowArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HW_11/Program.cs
@@ -0,0 +1,44 @@
+﻿using HW_11;
+
+CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+CancellationToken token = cancelTokenSource.Token;
+
+
+var array = new int[] { 73, 57, 49, 99, 133, 20, 1 };
+
+var sortFunction = new MergeSort();
+
+Console.WriteLine($"Array before sorting:");
+sortFunction.ShowArray(array);
+Console.WriteLine();
+
+Console.WriteLine($"Array after sorting:");
+Task task = new Task(() =>
+{
+
+    sortFunction.SortArray(array, 0, array.Length - 1);
+    if (token.IsCancellationRequested)
+    {
+        throw new TimeoutException();
+    }
+
+}, token);
+try
+{
+
+    task.Start();
+    cancelTokenSource.CancelAfter(100);
+    task.Wait();
+}
+catch (Exception)
+{
+    Console.WriteLine("\nTasks cancelled: timed out.\n");
+    return;
+}
+finally
+{
+    cancelTokenSource.Dispose();
+}
+
+sortFunction.ShowArray(array);
+Console.WriteLine();
